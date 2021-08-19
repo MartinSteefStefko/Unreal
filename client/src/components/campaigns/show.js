81f8948c006a238da68b7cd5ae5ba30d8ac89979
Martin Stefko <mato.stefko@gmail.com>
2021-08-19 09:53:40 +0200

    Initial CampaignShow component

--- client/src/components/campaigns/show.js
+++ client/src/components/campaigns/show.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
 import { Card, Grid, Button } from 'semantic-ui-react';
-import Layout from '../../components/Layout';
+// import Layout from '../../components/';
 import Campaign from '../../ethereum/campaign';
 import web3 from '../../ethereum/web3';
-import ContributeForm from '../../components/ContributeForm';
-import { Link } from '../../routes';
+import ContributeForm from '../../components/ContributeForm/ContributeForm';
+import { Route, Link } from 'react-router-dom';
+import { withRouter } from 'react-router';
 
 class CampaignShow extends Component {
-  static async getInitialProps(props) {
-    const campaign = Campaign(props.query.address);
+  // static async getInitialProps(props) {
+  //   const campaign = Campaign(props.query.address);
 
+  //   const summary = await campaign.methods.getSummary().call();
+
+  //   return {
+  //     address: props.query.address,
+  //     minimumContribution: summary[0],
+  //     balance: summary[1],
+  //     requestsCount: summary[2],
+  //     approversCount: summary[3],
+  //     manager: summary[4],
+  //   };
+  // }
+
+  state = {
+    address: '',
+    minimumContribution: 0,
+    balance: 0,
+    requestsCount: 0,
+    approversCount: 0,
+    manager: '',
+  };
+
+  // const match = useRouteMatch();
+  // const params = useParams();
+
+  // const { quoteId } = params;
+
+  async componentDidMount() {
+    const address = this.props.match.params.campaignAddress;
+    const campaign = Campaign(address);
     const summary = await campaign.methods.getSummary().call();
 
-    return {
-      address: props.query.address,
+    this.setState({
+      address: address,
       minimumContribution: summary[0],
       balance: summary[1],
       requestsCount: summary[2],
       approversCount: summary[3],
-      manager: summary[4]
-    };
+      manager: summary[4],
+    });
   }
 
   renderCards() {
@@ -28,8 +58,8 @@ class CampaignShow extends Component {
       manager,
       minimumContribution,
       requestsCount,
-      approversCount
-    } = this.props;
+      approversCount,
+    } = this.state;
 
     const items = [
       {
@@ -37,63 +67,70 @@ class CampaignShow extends Component {
         meta: 'Address of Manager',
         description:
           'The manager created this campaign and can create requests to withdraw money',
-        style: { overflowWrap: 'break-word' }
+        style: { overflowWrap: 'break-word' },
       },
       {
         header: minimumContribution,
         meta: 'Minimum Contribution (wei)',
         description:
-          'You must contribute at least this much wei to become an approver'
+          'You must contribute at least this much wei to become an approver',
       },
       {
         header: requestsCount,
         meta: 'Number of Requests',
         description:
-          'A request tries to withdraw money from the contract. Requests must be approved by approvers'
+          'A request tries to withdraw money from the contract. Requests must be approved by approvers',
       },
       {
         header: approversCount,
         meta: 'Number of Approvers',
         description:
-          'Number of people who have already donated to this campaign'
+          'Number of people who have already donated to this campaign',
       },
       {
         header: web3.utils.fromWei(balance, 'ether'),
         meta: 'Campaign Balance (ether)',
         description:
-          'The balance is how much money this campaign has left to spend.'
-      }
+          'The balance is how much money this campaign has left to spend.',
+      },
     ];
 
     return <Card.Group items={items} />;
   }
 
   render() {
+    const { match } = this.props;
+    const { state } = this.state;
     return (
-      <Layout>
+      // <Layout>
+      <div>
         <h3>Campaign Show</h3>
         <Grid>
           <Grid.Row>
             <Grid.Column width={10}>{this.renderCards()}</Grid.Column>
 
             <Grid.Column width={6}>
-              <ContributeForm address={this.props.address} />
+              <ContributeForm address={state.address} />
             </Grid.Column>
           </Grid.Row>
 
           <Grid.Row>
             <Grid.Column>
-              <Link route={`/campaigns/${this.props.address}/requests`}>
-                <a>
-                  <Button primary>View Requests</Button>
-                </a>
-              </Link>
+              {/* this will be nested route */}
+              <Route path={match.path} exact>
+                <Link to={`${match.url}/campaigns/${state.address}/requests`}>
+                  <a>
+                    <Button primary>View Requests</Button>
+                  </a>
+                </Link>
+              </Route>
             </Grid.Column>
           </Grid.Row>
         </Grid>
-      </Layout>
+      </div>
+      // </Layout>
     );
   }
 }
 
-export default CampaignShow;
+export default withRouter(CampaignShow);
